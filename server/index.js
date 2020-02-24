e528383964e0ac33e5d34afefe26fdd79e4b82c7
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-24 21:00:29 +0000

    changed msg on terminal to Tweeter App

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
 
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -30,5 +31,5 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 app.use("/tweets", tweetsRoutes);
 
 app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+  console.log("Tweeter app listening on port " + PORT);
 });
