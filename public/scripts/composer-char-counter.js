4375bd70addfb1514c42284fe655db6816b6a2c9
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-26 01:48:07 +0000

    made exceeding counter appear red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,29 @@ $(document).ready(function() {
 
 
     $("textarea").on('keyup', function() {
-        // let result = 140 - $(this).val().length;
-        $('.counter')[0].textContent = 140 - $(this).val().length;
-        console.log($('.counter')[0].textContent)
+        // // let difference = $(this).next().next()[0].textContent;
+        // let length = $(this).val().length;
+        // // let result = 140 - length;
+        // let counter = $(this).next().next()[0]
+        // let result = counter.textContent
+        // if (result.text < 0) {
+        //     counter.addClass("overlimit")
+        // }
+        // result = counter
 
-        
+        // console.log(counter) 
+        let length = $(this).val().length;
+        let counter = $(this).next().next();
+        let result = counter.text(140 - length);
+
+        if (counter.text() < 0) {
+            counter.addClass("overlimit")
+        }
+
+
+        console.log(result)
     });
+
+
 });
 
