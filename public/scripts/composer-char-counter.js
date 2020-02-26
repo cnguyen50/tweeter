d7503dbbc8e32fde0a5c40679ca7adb72387f875
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-26 22:33:11 +0000

    implemented text area counter and if over text turns red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,24 +2,15 @@ $(document).ready(function() {
     console.log("hello dere");
 
 
-    $("textarea").on('keyup', function() {
-        // // let difference = $(this).next().next()[0].textContent;
-        // let length = $(this).val().length;
-        // // let result = 140 - length;
-        // let counter = $(this).next().next()[0]
-        // let result = counter.textContent
-        // if (result.text < 0) {
-        //     counter.addClass("overlimit")
-        // }
-        // result = counter
-
-        // console.log(counter) 
+    $("textarea").on('input', function() {
         let length = $(this).val().length;
         let counter = $(this).next().next();
         let result = counter.text(140 - length);
 
         if (counter.text() < 0) {
             counter.addClass("overlimit")
+        } else {
+            counter.removeClass("overlimit")
         }
 
 
