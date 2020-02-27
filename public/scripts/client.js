f27980dba2ef52ce7ba831348355cfb32e05855a
Calvin Nguyen <calvinnguyen50@gmail.com>
2020-02-27 02:26:50 +0000

    added loadtweets functions for GET ajax method

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,30 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
 
 $(() => {
   const renderTweets = (tweetsArr) => {
@@ -36,26 +12,28 @@ $(() => {
     })};
     
   const $button = $("#tweet-form");
-  $button.on('submit', function () {
-  console.log("listening here", $button.serialize());
-  event.preventDefault();
-  $.ajax({
-    url: "/tweets",
-    method: "POST",
-    data: $button.serialize() 
-  })
-    .then(function () {
-      console.log("CREATED NEW TWEET");
+  $button.on('submit', () => {
+    console.log("listening here", $button.serialize());
+    event.preventDefault();
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $button.serialize() 
     })
-    
-    
-    
-
-
-
-
+      .then(function () {
+        console.log("CREATED NEW TWEET");
+      })
   });
 
+  const loadtweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET"
+    })
+      .then(tweets => {
+        renderTweets(tweets);
+    });
+  }
 
 
     
@@ -64,7 +42,7 @@ $(() => {
     const avatars = tweetData.user.avatars;
     const handle = tweetData.user.handle;
     const content = tweetData.content.text;
-    const createdAt = tweetData.created_at;
+    const createdAt = moment(tweetData.created_at).fromNow();
         
     return `
             <article class="tweet">
@@ -82,7 +60,8 @@ $(() => {
             </article>
         `;
   };
-  renderTweets(data);
+  // renderTweets(data);
+  loadtweets();
 });
 
 
